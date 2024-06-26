2a79044e5877bccd572e82b72e693aaf0b9aa561
Thanawich Pinprayoon <thanawich.pin@gmail.com>
2024-06-26 16:30:16 +0700

    add post  path param body

---
+++ locustfile.py
@@ -0,0 +1,44 @@
+from locust import HttpUser, TaskSet, task, between
+import json
+
+
+class WebsiteUser(HttpUser):
+    # we assume someone who is browsing the website,
+    # generally has some waiting time (between
+    # 5 and 10 seconds)
+    wait_time = between(0, 1)
+
+    def on_start(self):
+        # start by waiting so that the simulated users
+        # won't all arrive at the same time
+        self.wait()
+        # assume all users arrive at the index page
+        self.hello_world()
+        self.login()
+
+    def login(self):
+        response = self.client.post("/login", data={"username": "user", "password": "pass"})
+        self.token = response.json()["access_token"]
+
+    @task(1)
+    def hello_world(self):
+        self.client.get("/")
+
+    @task(5)
+    def auth_get(self):
+        self.client.get("/protected-get", headers={"Authorization": f"Bearer {self.token}"})
+
+    @task(5)
+    def auth_post(self):
+        self.client.post(
+            "/protected-post/test-path-1?param=test-param-1",
+            headers={
+                "Authorization": f"Bearer {self.token}"
+            },
+            json={
+                "name": "name-1",
+                "description": "description-1",
+                "price": 0,
+                "tax": 0
+            }
+        )
